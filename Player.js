4e94f06d9d2041dd83ce8b440255e30d607b7d2c
Daksh Sangal <134773611+Qu-Ack@users.noreply.github.com>
2023-07-02 07:53:37 -0400

    We completed the project we did not style it as we should have but we made the whole javascript code using modules and factory functions on our own without any help from google or bing~if you search there which I am proud of

--- Player.js
+++ Player.js
@@ -1,13 +1,27 @@
-const Player =  (choice)  => {
+// Factory function for our Player cause we have to make more than one player otherwise 
+// I would have used an IIFE.
+
+var choice = 'X';
+const Player =  ()  => {
+
+
+    
 
     function makeChoice() {
-        // player would be able to choose his weapon.
+        // player would be able to choose his weapon. 
+        // this would only happend once until the game ends
+    }
+
+    function win() { 
+        alert(`${choice} wins`)
+        location.reload()
     }
 
 
-    function changeBoard() {
-        let rows = 2
-        let cols = 1
+    function changeBoard(rows , cols) {
+        // these also will be interactive and the user will be able select them. 
+
+        // Empty or full logic in the board
         if (gameBoard.Board[rows][cols] == '') {
             gameBoard.Board[rows][cols] = choice
             gameBoard.render();
@@ -16,26 +30,73 @@ const Player =  (choice)  => {
         }
     }
 
-    return {changeBoard , makeChoice }
+    return {makeChoice, win , changeBoard}
 }
 
+// can't access these inside my handler module due to scope reasons
+// that is why have to create them globally the argument is just there for test purposes.
+var P1 = Player();
+var P2 = Player();
 
-var P1 = Player("O");
-var P2 = Player("X")
 
 
+// This function is responsible for managing other Modules and objects.
+// It will also be responsible for the basic flow of the game and I might add win logic here.
 var Handler = (function (player1 , player2) {
     var turn = true;
   
+
     function game() {
-      if (turn) {
-          player1.changeBoard();
-          turn = false;
+        if (turn) {
+            Listen(player1);
+        }
+        else {
+            Listen(player2 );
+        }
       }
-      else {
-          player2.changeBoard();
-          turn = true;
-      }
+
+    function Listen(player) {
+        gameBoard.cards.forEach(card => {
+            card.addEventListener('click' , function() {
+                if(choice == "X") {
+                    choice = "O"
+                }  
+                else if(choice == "O") {
+                    choice = "X"
+
+                } 
+                console.log(player.choice)
+                let row = card.getAttribute('data-row')
+                let col = card.getAttribute('data-col')
+                player.changeBoard(row , col)
+                
+                winLogic(player)
+                turn = false;
+               
+            })
+        })
+    }
+
+
+    
+
+
+    function winLogic(winner) {
+        for(let rows = 0 ; rows < gameBoard.Board.length ; rows++) {
+            if((gameBoard.Board[rows][0] == gameBoard.Board[rows][1] && gameBoard.Board[rows][0] == gameBoard.Board[rows][2]) && gameBoard.Board[rows][0] == choice) {
+                winner.win();
+            }
+        }
+
+        for(let cols = 0; cols < 3; cols++) {
+            if((gameBoard.Board[0][cols] == gameBoard.Board[1][cols] && gameBoard.Board[0][cols] == gameBoard.Board[2][cols]) && gameBoard.Board[0][cols] == choice) {
+                winner.win();
+            }
+        }
+
+        if(((gameBoard.Board[0][0] == gameBoard.Board[1][1] && gameBoard.Board[0][0] == gameBoard.Board[2][2]) && gameBoard.Board[0][0] == choice) || ((gameBoard.Board[0][2] == gameBoard.Board[1][1] && gameBoard.Board[0][2] == gameBoard.Board[2][0]) && gameBoard.Board[0][2] == choice)) {
+            winner.win();
+        }
     }
 
     return {game}
--- app.js
+++ app.js
@@ -2,10 +2,21 @@ var gameBoard = (function () {
     Board = [['' , '' , ''] , 
              ['' , '' , ''] ,
              ['' , '' , '']]
-    
+
+    //cache dom 
+    const cardContainer = document.querySelector('.grid-container');
+    const cards = document.querySelectorAll('.card');
+
+
+    // A function just to display this Board On screen this will later have DOM elements included
     function render() {
-        console.log(Board);
+        for(let i =0; i < 3; i++) {
+            for(let j = 0 ; j < 3 ; j++) {
+                let card = document.querySelector(`[data-row = "${i}"][data-col = "${j}"]`)
+                card.textContent = Board[i][j]
+            } 
+        }
     }
 
-    return {render , Board}
-})()
+    return {render , Board , cards}
+})() 
